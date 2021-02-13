2bf33581e5198ef216ba5a7f30720c6c83e00f70
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-13 18:14:27 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception #クロスサイトリクエストフォージェリ (CSRF)への対応策のコード
+
+  before_action :configure_permitted_parameters, if: :devise_controller? #configure_permitted_parametersというメソッドは、devise_controllerを使うときしか処理しないということをApplicationコントローラーを読み込む前に判断
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
