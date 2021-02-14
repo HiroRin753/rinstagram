63143bfb8bb134aa18f288d92f4b7c303d9f084f
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-14 15:59:30 +0900

    投稿画面編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,controllers:{registrations:'registrations'}
-  root 'pages#home'
+  root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user' #asオプションを使うと、ルーティングに名前をつけることができる
   resources :posts, only: %i(new create index)do
