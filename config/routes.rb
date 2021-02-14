6b23550412dc787f3aea6fcf8f0e3516a32b015b
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-14 16:11:52 +0900

    投稿詳細画面　リンク

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get '/users/:id', to: 'users#show', as: 'user' #asオプションを使うと、ルーティングに名前をつけることができる
-  resources :posts, only: %i(new create index)do
+  resources :posts, only: %i(new create index show)do
     resources :photos, only: %i(create)
   end
  
