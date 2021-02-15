8bc5c8f829564887da4587ede292469da2f7c52f
HiroRin753 <chikuzen.0306@gmail.com>
2021-02-15 21:59:44 +0900

    完了

--- db/migrate/20210215070109_add_user_name_to_user.rb
+++ db/migrate/20210215070109_add_user_name_to_user.rb
@@ -1,5 +1,5 @@
 class AddUserNameToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :username, :string, null:false
+    add_column :users, :username, :string, null:false, default:""
   end
 end
